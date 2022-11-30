5b5fea151af2996a29146e5e75e316b87e6f6f56
Glitch (glitch-hello-website) <none>
2022-11-30 04:26:09 +0000

    🎠💋 Checkpoint ./mask-2.js:49057023/909

--- mask-2.js
+++ mask-2.js
@@ -1,15 +1,15 @@
 /* globals allMasks  Vector2D, drawRibbon, drawContour, drawPoints */
 
-allMasks["sad guy"] = {
-  title: "a guy with watery eyes",
-  description: "idk",
+allMasks["white walker"] = {
+  title: "a white walker from game of thrones",
+  description: "a white walker from game of thrones",
   setup() {
     // p.clear()
   },
 
   draw(p, face) {
     p.clear();
-    p.background(40, 100, 47, 0.5);
+    
     let t = p.millis() * 0.001;
 
     face.sides.forEach((side, sideIndex) => {
@@ -27,22 +27,9 @@ allMasks["sad guy"] = {
       p.vertex(...face.bottom);
       p.endShape(p.CLOSE);
 
-      //sides of the flag
-      p.noStroke();
-      p.fill(...this.color);
-      side.face.forEach((contour, cIndex) => {
-        drawContour(p, contour, {
-          curve: true,
-          close: true,
-          lerpToPoint: face.bottom,
-          // lerpPct: Math.sin(t),
-          lerpPct(index, pct, pt) {
-            return 0.2 * Math.sin(pct * 45 + 10 * t) - 1.2;
-          },
-        });
-      });
-      side.eye.forEach((contour, cIndex) => {
-        p.fill(200, 100, 100 - cIndex * 17);
+      
+      side.eye.forEach((contour, i) => {
+        p.fill(200, 100, 100 - i * 17);
         drawContour(p, contour, {
           close: true,
           curve: true,
@@ -54,8 +41,8 @@ allMasks["sad guy"] = {
       });
       
     });
-    face.mouth.forEach((contour, cIndex) => {
-      p.fill(200, 0, 100 - cIndex * 17);
+    face.mouth.forEach((contour, i) => {
+      p.fill(200,100, 100 - i * 35,1);
       drawContour(p, contour, {
         close: true,
         curve: true,
