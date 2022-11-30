4d65bc144850e06b4ca6c159a12a9730d428876a
Glitch (glitch-hello-website) <none>
2022-11-30 04:03:06 +0000

    💪🐊 Checkpoint ./mask-kate.js:49057023/2111

--- mask-kate.js
+++ mask-kate.js
@@ -1,7 +1,7 @@
 /* globals allMasks  Vector2D, drawContour */
 
-allMasks["kate"] = {
-  title: "Kate's in-class mask",
+allMasks["clown mask"] = {
+  title: "a bad pennywise",
   description: "A bunch of techniques",
   setup(p) {
     p.clear()
@@ -13,18 +13,27 @@ allMasks["kate"] = {
     p.background(0, 0, 0, .01);
     let t = p.millis() * 0.001;
 
+  
     face.sides.forEach((side, sideIndex) => {
-      p.fill(100);
-      p.circle(...side.eyeCenter, side.eyeWidth * 0.2);
-      p.circle(...side.eyeInner, 3);
-      p.circle(...side.eyeOuter, 3);
+      p.fill(100)
+      p.stroke(100)
+      drawContour(p, side.face[0], {
+        close: true, 
+        curve: false
+      })
+      
+      p.fill(0);
+      p.stroke(0)
+      p.circle(...side.eyeCenter, 10);
+      // p.circle(...side.eyeInner, 3);
+      // p.circle(...side.eyeOuter, 3);
       
       
       p.fill(360,100,50,1)
       p.circle(...side.eyeTop, 3);
       p.circle(...side.eyeBottom, 3);
 
-      p.fill(150, 100, 50, 1);
+      
       p.stroke(0);
       
        
@@ -46,10 +55,23 @@ allMasks["kate"] = {
       });
     });
     
-    p.fill(300,100,50,1)
+    
+    
     p.circle(...face.bottom, 20);
     p.circle(...face.nose, 20);
-    p.circle(...face.top, 20);
+    
+    drawContour(p, face.mouth[0], {
+        close: true,
+        lerpToPoint: face.bottom,
+        lerpPct: p.noise(3*t, 0)
+      })
+    
+    p.fill(0)
+    drawContour(p, face.mouth[1], {
+        close: true,
+        lerpToPoint: face.bottom,
+        lerpPct: p.noise(3*t, 0)
+      })
      
   },
 };
